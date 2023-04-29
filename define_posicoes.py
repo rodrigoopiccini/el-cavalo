2ea810c44dd6efdb0df0b8605de733d54753f547
rodrigoopiccini <rodrigopiccini12@gmail.com>
2023-04-29 17:09:05 -0300

    primeiro exercicio - define posições

--- define_posicoes.py
+++ define_posicoes.py
@@ -1,1 +1,21 @@
-print ('ok')
+def define_posicoes (linha, coluna, orientacao, tamanho):
+    lista = []
+
+    if orientacao == 'vertical':
+        for i in range(tamanho):
+            posicao = []
+            posicao.append(linha+i)
+            posicao.append(coluna)
+            lista.append(posicao)
+
+    if orientacao == 'horizontal':
+        for i in range(tamanho):
+            posicao = []
+            posicao.append(linha)
+            posicao.append(coluna+i)
+            lista.append(posicao)
+
+
+    return lista
+    
+print (define_posicoes (2, 4, 'horizontal', 3))
