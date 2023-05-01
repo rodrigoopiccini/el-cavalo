f1bfff6675349940aa0a4df630a169ed331591b5
rodrigoopiccini <rodrigopiccini12@gmail.com>
2023-05-01 20:07:05 -0300

    posicionando frota e anteriores

--- batalha_naval.py
+++ batalha_naval.py
@@ -75,3 +75,62 @@ def afundados (frota, tabuleiro):
     
     return contador
 
+# 6 - posição válida
+def posicao_valida (frota, linha, coluna, orientacao, tamanho):
+    posicoes = define_posicoes (linha, coluna, orientacao, tamanho)
+
+    if orientacao == 'vertical' and 10 - linha < tamanho:
+        return False
+    
+    if orientacao == 'horizontal' and 10 - coluna < tamanho:
+        return False
+    
+
+    for posicao in posicoes:
+        linha = posicao[0]
+        coluna = posicao[1]
+
+        for tipo, posicoes in frota.items():
+            for posicao in posicoes:
+                for linha_frota, coluna_frota in posicao:
+                    if linha == linha_frota and coluna == coluna_frota:
+                        return False
+                    
+    return True
+
+# 7 - posicionando frota
+embarcacoes = {1: ['porta-aviões', 4], 2:['navio-tanque', 3], 3:['navio-tanque', 3], 4:['contratorpedeiro', 2], 5:['contratorpedeiro', 2], 6:['contratorpedeiro', 2], 7: ['submarino',1], 8: ['submarino',1], 9: ['submarino',1], 10: ['submarino',1]}
+frota = {}
+i = 1
+while i <= len(embarcacoes):
+    nome = embarcacoes[i][0]
+    tamanho = embarcacoes[i][1]
+    
+    print ('Insira as informações referentes ao navio {0} que possui tamanho {1}'.format(nome, tamanho))
+    linha = int(input('Linha: '))
+    coluna = int(input('Coluna: '))
+
+    if nome != 'submarino':
+        orientacao = int(input('Vertical(1) ou Horizontal(2): '))
+        
+    if orientacao == 1:
+        orientacao = 'vertical'
+    elif orientacao == 2:
+        orientacao = 'horizontal'
+
+    valido = posicao_valida (frota, linha, coluna, orientacao, tamanho)
+
+    if valido == False:
+        print('Esta posição não está válida!')
+    
+    else:
+        lista_posicoes = define_posicoes (linha, coluna, orientacao, tamanho)
+        frota = preenche_frota (frota, nome, linha, coluna, orientacao, tamanho)
+        i += 1
+
+print (frota)
+
+
+
+
+        
