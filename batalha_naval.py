3149f2e8dc3930521cc569d91b211d2f13cac8cf
rodrigoopiccini <rodrigopiccini12@gmail.com>
2023-05-01 22:48:13 -0300

    finalizado

--- batalha_naval.py
+++ batalha_naval.py
@@ -1,4 +1,6 @@
 # 1 - Define Posições
+import random
+
 def define_posicoes (linha, coluna, orientacao, tamanho):
     lista = []
 
@@ -98,6 +100,7 @@ def posicao_valida (frota, linha, coluna, orientacao, tamanho):
                     
     return True
 
+
 # 7 - posicionando frota
 embarcacoes = {1: ['porta-aviões', 4], 2:['navio-tanque', 3], 3:['navio-tanque', 3], 4:['contratorpedeiro', 2], 5:['contratorpedeiro', 2], 6:['contratorpedeiro', 2], 7: ['submarino',1], 8: ['submarino',1], 9: ['submarino',1], 10: ['submarino',1]}
 frota = {}
@@ -128,9 +131,121 @@ while i <= len(embarcacoes):
         frota = preenche_frota (frota, nome, linha, coluna, orientacao, tamanho)
         i += 1
 
-print (frota)
+# 8 - jogadas do jogador
+frota_oponente = {
+    'porta-aviões': [
+        [[9, 1], [9, 2], [9, 3], [9, 4]]
+    ],
+    'navio-tanque': [
+        [[6, 0], [6, 1], [6, 2]],
+        [[4, 3], [5, 3], [6, 3]]
+    ],
+    'contratorpedeiro': [
+        [[1, 6], [1, 7]],
+        [[0, 5], [1, 5]],
+        [[3, 6], [3, 7]]
+    ],
+    'submarino': [
+        [[2, 7]],
+        [[0, 6]],
+        [[9, 7]],
+        [[7, 6]]
+    ]
+}
 
+tabuleiro_oponente = posiciona_frota(frota_oponente)
+tabuleiro_jogador = posiciona_frota(frota) 
 
-
-
+lista_linha_coluna_ataque_anterior = []
+jogando = True
+while jogando:
+    def monta_tabuleiros(tabuleiro_jogador, tabuleiro_oponente):
+        texto = ''
+        texto += '   0  1  2  3  4  5  6  7  8  9         0  1  2  3  4  5  6  7  8  9\n'
+        texto += '_______________________________      _______________________________\n'
         
+        for linha in range(len(tabuleiro_jogador)):
+            jogador_info = '  '.join([str(item) for item in tabuleiro_jogador[linha]])
+            oponente_info = '  '.join([info if str(info) in 'X-' else '0' for info in tabuleiro_oponente[linha]])
+            texto += f'{linha}| {jogador_info}|     {linha}| {oponente_info}|\n'
+            
+        return texto
+    
+    print (monta_tabuleiros(tabuleiro_jogador, tabuleiro_oponente))
+
+    linha_coluna_invalida = True
+    while linha_coluna_invalida:
+
+        linha_invalida = True
+
+        while linha_invalida:
+            linha_atacar = int(input('Linha de ataque: '))
+            if linha_atacar > 9 or linha_atacar < 0:
+                print ('Linha inválida!')
+                
+            else:
+                linha_invalida = False
+        
+        coluna_invalida = True
+    
+        while coluna_invalida:
+            coluna_atacar = int(input('Coluna de ataque: '))
+            if coluna_atacar > 9 or coluna_atacar < 0:
+                print ('Coluna inválida!')
+                
+            else:
+                coluna_invalida = False
+        
+        lista_linha_coluna_ataque = [linha_atacar]
+        lista_linha_coluna_ataque. append(coluna_atacar)
+        
+        if lista_linha_coluna_ataque in lista_linha_coluna_ataque_anterior:
+            print ('A posição linha {0} e coluna {1} já foi informada anteriormente!'.format(linha_atacar, coluna_atacar))
+            
+        else:
+            lista_linha_coluna_ataque_anterior.append(lista_linha_coluna_ataque)
+            linha_coluna_invalida = False
+
+    novo_tabuleiro = faz_jogada(tabuleiro_oponente, linha_atacar, coluna_atacar)
+    quantos_afundados = afundados (frota_oponente, novo_tabuleiro)
+
+    if quantos_afundados == 10:
+        print ('Parabéns! Você derrubou todos os navios do seu oponente!')
+        jogando = False
+
+# jogadas do oponente
+    lista_linha_coluna_ataque_oponente_anterior = []
+
+    linha_coluna_oponente_invalida = True
+    while linha_coluna_oponente_invalida:
+        linha_ataque_oponente = random.randint(0,9)
+        coluna_ataque_oponente = random.randint(0,9)
+        
+        lista_linha_coluna_ataque_oponente = [linha_ataque_oponente]
+        lista_linha_coluna_ataque_oponente.append(coluna_ataque_oponente)
+        
+        if lista_linha_coluna_ataque_oponente not in lista_linha_coluna_ataque_oponente_anterior:
+            lista_linha_coluna_ataque_oponente_anterior.append(lista_linha_coluna_ataque_oponente)
+            linha_coluna_oponente_invalida = False
+            print('Seu oponente está atacando na linha {0} e coluna {1}'.format(linha_ataque_oponente, coluna_ataque_oponente))
+
+    novo_tabuleiro = faz_jogada(tabuleiro_jogador, linha_ataque_oponente, coluna_ataque_oponente)
+    quantos_afundados = afundados (frota, novo_tabuleiro)
+
+    if quantos_afundados == 10:
+        print ('Xi! O oponente derrubou toda a sua frota =(')
+        jogando = False
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
